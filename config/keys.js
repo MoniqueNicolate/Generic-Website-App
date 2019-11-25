2e4588647539117975b9b1945bd636e168dd2f94
Captaincheq <alecmabhizachirawu@gmail.com>
2019-11-25 21:43:06 +0800

    update

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Alec:Chirawu123412@captaincheqcluster-ds7km.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://mumu:Musa@1997@mumucluster-2rjdz.mongodb.net/test?retryWrites=true'
 };
